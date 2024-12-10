6e679ffc01370954144df9928709c79970858ad9
JoaquinCodeGit <h.j.caceres1990@gmail.com>
2024-12-10 11:47:04 -0300

    Realizamos la primer app

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("hola peton");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese algo");
+
+        String linea = scanner.nextLine();
+
+        System.out.println("Dijiste: " + linea);
+
+        scanner.close();
+
+    }
+}
